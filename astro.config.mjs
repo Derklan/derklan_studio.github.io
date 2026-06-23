43c5e19ac1c3b4b26f3f59e3028b4aaef538f10d
mathys le juez {Dark Juezu} <56997133+le-juez-mathys@users.noreply.github.com>
2026-06-23 15:09:33 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // MODIFIER : Remplace par ton URL GitHub Pages
-  site: 'https://ton-org.github.io',
+  site: 'https://derklan.github.io/Derklan-Studio.github.io/',
 });
